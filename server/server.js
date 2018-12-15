5892073b0ce216e869589b68c1da2e5126ec6246
Travis Lang <tlang50@icloud.com>
2018-12-15 12:36:42 -0600

    setup basic route structure

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.json()); 
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use()
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Header from '../Header/Header';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div className="App">
-        <p>Empty Page</p>
-      </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Route path='/' exact componenet={Projects} />
+                <Route path='/admin' componenet={Projects} />
+            </div>
+        </Router>
     );
   }
 }
